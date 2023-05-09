f58d2a83e9f75b4fb395807180216281ffcc30f2
Shirolin <jerrys40506@gmail.com>
2023-05-09 11:08:03 +0900

    Props use Card by props and v-for

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,7 +1,10 @@
 <script>
 export default {
-    props: [
+    props: [//設定變數給其他vue做v-bind
         "cardTitle",
+        "localImg",
+        "message",
+        "buttonTitle"
     ]
 }
 </script>
@@ -9,23 +12,25 @@ export default {
     <div>
         <div class="card">
             <h3>{{ cardTitle }}</h3>
-            <img src="" alt="">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-            <button type="button">Go</button>
+            <img :src="localImg">
+            <p>{{ message }}</p>
+            <button type="button">{{ buttonTitle }}</button>
         </div>
     </div>
 </template>
 <style lang="scss" scoped>
-    .card{
-        border:1px solid black;
-        border-radius: 5px;
-        padding: 12px;
-        margin: 12px;
-        max-width: 250px;
-        min-width: 150px;
-        img{
-            width: 100%;
-            height: 100px;
-        }
+.card {
+    border: 1px solid black;
+    border-radius: 5px;
+    padding: 12px;
+    margin: 12px;
+    max-width: 250px;
+    min-width: 150px;
+
+    img {
+        width: 100%;
+        height: 100px;
+        object-fit: cover;
     }
+}
 </style>
--- src/views/Props.vue
+++ src/views/Props.vue
@@ -6,7 +6,44 @@ export default {
     },
     data() {
         return {
-           
+            localImgArr: [
+                {
+                    img: "../../public/img/area2_img1.png",
+                    title: "A2圖1",
+                    message: "msg1",
+                    buttonTitle: "Go",
+                },
+                {
+                    img: "../../public/img/area2_img2.png",
+                    title: "A2圖2",
+                    message: "msg2",
+                    buttonTitle: "Go",
+                },
+                {
+                    img: "../../public/img/area2_img3.png",
+                    title: "A2圖3",
+                    message: "msg3",
+                    buttonTitle: "Go",
+                },
+                {
+                    img: "../../public/img/area3_img1.png",
+                    title: "A3圖1",
+                    message: "msg4",
+                    buttonTitle: "Go",
+                },
+                {
+                    img: "../../public/img/area3_img2.png",
+                    title: "A3圖2",
+                    message: "msg5",
+                    buttonTitle: "Go",
+                },
+                {
+                    img: "../../public/img/area3_img3.png",
+                    title: "A3圖3",
+                    message: "msg6",
+                    buttonTitle: "Go",
+                },
+            ],
         }
     },
 }
@@ -16,9 +53,8 @@ export default {
         <h2>這裡是Props</h2>
     </div>
     <div class="props">
-        <Card :cardTitle="'title'" />
-        <Card :cardTitle="'title2'" />
-        <Card :cardTitle="'title3'" />
+        <Card v-for="data in localImgArr" :cardTitle="data.title" :localImg="data.img" :message="data.message"
+            :buttonTitle="data.buttonTitle" />
 
     </div>
 </template>
